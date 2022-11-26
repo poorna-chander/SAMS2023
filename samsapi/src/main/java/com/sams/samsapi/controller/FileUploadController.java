aae3cbe36a79e222b14c13a7bef7a5b7b8d01f5b
poorna-chander <p.poornachander@live.com>
2022-11-26 09:46:37 -0500

    added upload download util basecode

---
+++ samsapi/src/main/java/com/sams/samsapi/controller/FileUploadController.java
@@ -0,0 +1,59 @@
+package com.sams.samsapi.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sams.samsapi.service.FileUploadService;
+
+@RestController
+@RequestMapping("/")
+public class FileUploadController {
+    @Autowired
+    FileUploadService fileuploadservice;
+
+    @PostMapping("/upload")
+    public void uploadFile(@Value("${filelocation.savepath}") String uploadPath, MultipartFile file)
+            throws IllegalStateException, IOException {
+        fileuploadservice.uploadFile(uploadPath, file);
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Object> downloadFile(@Value("${filelocation.savepath}") String filepath,
+            @PathVariable String filename)
+            throws FileNotFoundException {
+
+        File file = fileuploadservice.downloadFile(filepath, filename);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition",
+                String.format("attachment; filename=\"%s\"", file.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        ResponseEntity<Object> responseEntity = ResponseEntity.ok().headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/txt")).body(resource);
+
+        return responseEntity;
+    }
+
+}
---
+++ samsapi/src/main/java/com/sams/samsapi/service/FileUploadService.java
@@ -0,0 +1,25 @@
+package com.sams.samsapi.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileUploadService {
+    public void uploadFile(String uploadPath, MultipartFile file) throws IllegalStateException, IOException {
+        file.transferTo(new File(uploadPath + file.getOriginalFilename()));
+    }
+
+    public File downloadFile(String filePath, String filename) {
+
+        File file = new File(filePath + filename);
+
+        return file;
+
+    }
+}
