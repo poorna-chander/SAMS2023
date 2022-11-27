ecbe30036abe7f5bdc7770a73bfbf6d02ce0600c
poorna-chander <p.poornachander@live.com>
2022-11-27 18:09:24 -0500

    REVISION COMPONENT

--- samsui/src/app/app-routing.module.ts
+++ samsui/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingUserComponent } from './landing-user/landing-user.component';
+import { LoginComponent } from './login/login.component';
+import { SubmitterFormComponent } from './submitter-form/submitter-form.component';
+import { SubmitterRevisionComponent } from './submitter-revision/submitter-revision.component';
+import { SubmitterSubmissionsComponent } from './submitter-submissions/submitter-submissions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'landing', component: LandingUserComponent },
+  { path: 'revise/:paperId', component: SubmitterRevisionComponent }
+];
+
+
+export const routing = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
