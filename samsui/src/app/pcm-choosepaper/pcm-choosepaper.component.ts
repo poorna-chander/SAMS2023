bbd98f0d59f955914dab2a91b82095973fc0c83e
poorna-chander <p.poornachander@live.com>
2022-12-02 14:24:24 -0500

    choose papers PCM

--- samsui/src/app/pcm-choosepaper/pcm-choosepaper.component.ts
+++ samsui/src/app/pcm-choosepaper/pcm-choosepaper.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Papers {
+  title: string;
+  revision: number;
+  paperId: number;
+}
+
+const paperData: Papers[] = [
+  {title: "model driven development", revision: 5, paperId: 1},
+  {title: "collaborative software development", revision: 5, paperId: 1},
+  {title: "software construction", revision: 5, paperId: 1},
+  {title: "SMAR", revision: 5, paperId: 1}
+];
+
 @Component({
   selector: 'app-pcm-choosepaper',
   templateUrl: './pcm-choosepaper.component.html',
@@ -7,9 +20,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PcmChoosepaperComponent implements OnInit {
 
+  displayedColumns: string[] = ['title', 'revision', 'paperId', "choose"];
+  paperDetails: any[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.paperDetails = paperData;
   }
 
 }
